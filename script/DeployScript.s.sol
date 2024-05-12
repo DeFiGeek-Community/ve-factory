304deaca57d409e89401c1c8d260f8e3b6d09bcc
naizo10 <naizo018767@gmail.com>
2024-05-13 01:14:06 +0900

    Update DeployScript to use UUPS proxy for VeTokenFactory

--- script/DeployScript.s.sol
+++ script/DeployScript.s.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.24;
 
 import "forge-std/Script.sol";
+import "forge-std/console.sol";
+import "lib/openzeppelin-foundry-upgrades/src/Upgrades.sol";
 import "../src/VeTokenFactory.sol";
 
 contract DeployVeTokenFactory is Script {
-    VeTokenFactory public veFactory;
-
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
@@ -17,7 +17,20 @@ contract DeployVeTokenFactory is Script {
     }
 
     function deploy() internal {
-        veFactory = new VeTokenFactory();
+
+        address initialOwner = vm.envAddress("INITIAL_OWNER");
+        // VeTokenFactoryの初期化関数とその引数をエンコード
+        bytes memory initializerData = abi.encodeWithSelector(
+            VeTokenFactory.initialize.selector,
+            initialOwner // ここに初期所有者のアドレスを指定
+        );
+
+        // UUPSプロキシとしてVeTokenFactoryをデプロイ
+        address proxyAddress = Upgrades.deployUUPSProxy(
+            "src/VeTokenFactory.sol:VeTokenFactory",
+            initializerData
+        );
+        console.log("Deployed VeTokenFactory proxy at:", proxyAddress);
     }
 }
 
