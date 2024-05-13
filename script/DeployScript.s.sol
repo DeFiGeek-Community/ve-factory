0bf51a09369f6fb321c0ecebc325d43ef293a10b
naizo10 <naizo018767@gmail.com>
2024-05-13 17:15:19 +0900

    Update deploy function and import paths in DeployScript

--- script/DeployScript.s.sol
+++ script/DeployScript.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
 import "lib/openzeppelin-foundry-upgrades/src/Upgrades.sol";
-import "../src/VeTokenFactory.sol";
+import "src/VeTokenFactory.sol";
 
 contract DeployVeTokenFactory is Script {
     function run() external {
@@ -16,21 +16,21 @@ contract DeployVeTokenFactory is Script {
         vm.stopBroadcast();
     }
 
-    function deploy() internal {
-
-        address initialOwner = vm.envAddress("INITIAL_OWNER");
+    function deploy() internal returns (address) {
+        address initialOwner = msg.sender;
         // VeTokenFactoryの初期化関数とその引数をエンコード
-        bytes memory initializerData = abi.encodeWithSelector(
-            VeTokenFactory.initialize.selector,
-            initialOwner // ここに初期所有者のアドレスを指定
+        bytes memory initializerData = abi.encodeCall(
+            VeTokenFactory.initialize,
+            (initialOwner)
         );
 
         // UUPSプロキシとしてVeTokenFactoryをデプロイ
         address proxyAddress = Upgrades.deployUUPSProxy(
-            "src/VeTokenFactory.sol:VeTokenFactory",
+            "VeTokenFactory.sol",
             initializerData
         );
-        console.log("Deployed VeTokenFactory proxy at:", proxyAddress);
+        // console.log("Deployed VeTokenFactory proxy at:", proxyAddress);
+        return proxyAddress;
     }
 }
 
