7d673e12ce2bb70d0c016657c43c27fdbb5f0fe7
naizo10 <naizo018767@gmail.com>
2024-05-24 18:30:10 +0900

    Add Token.sol for mining and minting operations

---
+++ src/Token.sol
@@ -0,0 +1,284 @@
+pragma solidity ^0.8.24;
+
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright (C) 2024 DeFiGeek Community Japan
+ */
+
+//solhint-disable max-line-length
+//solhint-disable no-inline-assembly
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+/**
+ * @title Token Token
+ * @notice Divident. Inflatable but the rate is to be decreasing.
+ */
+contract Token is ERC20Permit {
+    event UpdateMiningParameters(uint256 time, uint256 rate, uint256 supply);
+    event SetMinter(address tokenMinter);
+    event SetAdmin(address admin);
+
+    address public tokenMinter;
+    address public admin;
+
+    // General constants
+    uint256 constant YEAR = 365 days;
+    uint256 constant INFLATION_DELAY = 1 days;
+
+    // Supply parameters
+    uint256 public initialRate;
+    uint256 public rateReductionTime;
+    uint256 public rateReductionCoefficient;
+    uint256 public rateDenominator;
+    uint256 public inflationDelay;
+
+    // Supply variables
+    int128 public miningEpoch;
+    uint256 public startEpochTime;
+    uint256 public rate;
+
+    uint256 startEpochSupply;
+    uint256 public startTime;
+
+    uint8 internal _decimals;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals_,
+        uint256 initialSupply_,
+        uint256 initialRate_,
+        uint256 rateReductionTime_,
+        uint256 rateReductionCoefficient_,
+        uint256 inflationDelay_
+    ) ERC20Permit(name) ERC20(name, symbol) {
+    require(decimals_ >= 4 && decimals_ <= 18, "decimals must be between 4 and 18"); // この行を修正
+        _decimals = decimals_;
+        uint256 _initialSupply = initialSupply_ * (10 ** uint256(decimals_));
+        _mint(msg.sender, _initialSupply);
+        admin = msg.sender;
+
+        initialRate =
+            (initialRate_ * (10 ** uint256(decimals_))) /
+            rateReductionTime_;
+        rateReductionTime = rateReductionTime_;
+        rateReductionCoefficient =
+            ((100 * (10 ** uint256(decimals_))) /
+            (100 - rateReductionCoefficient_));
+        rateDenominator = 10 ** uint256(decimals_);
+        inflationDelay = inflationDelay_;
+
+        startEpochTime = block.timestamp + inflationDelay - rateReductionTime;
+        startTime = block.timestamp;
+        miningEpoch = -1;
+        rate = 0;
+        startEpochSupply = _initialSupply;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+
+    /**
+     * @dev Update mining rate and supply at the start of the epoch
+     *      Any modifying mining call must also call this
+     */
+    function _updateMiningParameters() internal {
+        uint256 _rate = rate;
+        uint256 _startEpochSupply = startEpochSupply;
+
+        startEpochTime += rateReductionTime;
+        miningEpoch += 1;
+
+        if (_rate == 0 && miningEpoch < 1) {
+            _rate = initialRate;
+        } else {
+            _startEpochSupply += _rate * rateReductionTime;
+            startEpochSupply = _startEpochSupply;
+            _rate = (_rate * rateDenominator) / rateReductionCoefficient;
+        }
+
+        rate = _rate;
+
+        emit UpdateMiningParameters(block.timestamp, _rate, _startEpochSupply);
+    }
+
+    /**
+     * @notice Update mining rate and supply at the start of the epoch
+     * @dev Callable by any address, but only once per epoch
+     *      Total supply becomes slightly larger if(this function is called late
+     */
+    function updateMiningParameters() external {
+        require(
+            block.timestamp >= startEpochTime + rateReductionTime,
+            "dev: too soon!"
+        ); // dev: too soon!
+        _updateMiningParameters();
+    }
+
+    /**
+     * @notice Get timestamp of the current mining epoch start
+     *         while simultaneously updating mining parameters
+     * @return Timestamp of the epoch
+     */
+    function startEpochTimeWrite() external returns (uint256) {
+        uint256 _startEpochTime = startEpochTime;
+        if (block.timestamp >= _startEpochTime + rateReductionTime) {
+            _updateMiningParameters();
+            return startEpochTime;
+        } else {
+            return _startEpochTime;
+        }
+    }
+
+    /**
+     * @notice Get timestamp of the next mining epoch start
+     *         while simultaneously updating mining parameters
+     * @return Timestamp of the next epoch
+     */
+    function futureEpochTimeWrite() external returns (uint256) {
+        uint256 _startEpochTime = startEpochTime;
+        if (block.timestamp >= _startEpochTime + rateReductionTime) {
+            _updateMiningParameters();
+            return startEpochTime + rateReductionTime;
+        } else {
+            return _startEpochTime + rateReductionTime;
+        }
+    }
+
+    function _availableSupply() internal view returns (uint256) {
+        return startEpochSupply + (block.timestamp - startEpochTime) * rate;
+    }
+
+    // @notice Current number of tokens in existence (claimed or unclaimed)
+    function availableSupply() external view returns (uint256) {
+        return _availableSupply();
+    }
+
+    /**
+     * @notice How much supply is mintable from start timestamp till end timestamp
+     * @param start Start of the time interval (timestamp)
+     * @param end End of the time interval (timestamp)
+     * @return Tokens mintable from `start` till `end`
+     */
+    function mintableInTimeframe(
+        uint256 start,
+        uint256 end
+    ) external view returns (uint256) {
+        require(start <= end, "dev: start > end"); // dev: start > end
+        uint256 to_mint = 0;
+        uint256 currentEpochTime = startEpochTime;
+        uint256 currentRate = rate;
+
+        // Special case if(end is in future (not yet minted) epoch
+        if (end > currentEpochTime + rateReductionTime) {
+            currentEpochTime += rateReductionTime;
+            currentRate =
+                (currentRate * rateDenominator) /
+                rateReductionCoefficient;
+        }
+
+        require(
+            end <= currentEpochTime + rateReductionTime,
+            "dev: too far in future"
+        ); // dev: too far in future
+
+        // Curve will not work in 1000 years. Darn!
+        for (uint i; i < 999; ) {
+            if (end >= currentEpochTime) {
+                uint256 currentEnd = end;
+                if (currentEnd > currentEpochTime + rateReductionTime) {
+                    currentEnd = currentEpochTime + rateReductionTime;
+                }
+
+                uint256 currentStart = start;
+                if (currentStart >= currentEpochTime + rateReductionTime) {
+                    break; // We should never get here but what if...
+                } else if (currentStart < currentEpochTime) {
+                    currentStart = currentEpochTime;
+                }
+                to_mint += currentRate * (currentEnd - currentStart);
+                if (start >= currentEpochTime) {
+                    break;
+                }
+            }
+
+            currentEpochTime -= rateReductionTime;
+            currentRate =
+                (currentRate * rateReductionCoefficient) /
+                rateDenominator; // double-division with rounding made rate a bit less => good
+            require(currentRate <= initialRate, "This should never happen"); // This should never happen
+
+            unchecked {
+                i++;
+            }
+        }
+
+        return to_mint;
+    }
+
+    /**
+     * @notice Set the tokenMinter address
+     * @dev Only callable once, when tokenMinter has not yet been set
+     * @param _tokenMinter Address of the tokenMinter
+     */
+    function setMinter(address _tokenMinter) external onlyAdmin {
+        require(
+            _tokenMinter != address(0),
+            "dev: can set the tokenMinter only once, at creation"
+        ); // dev: can set the tokenMinter only once, at creation
+        tokenMinter = _tokenMinter;
+        emit SetMinter(_tokenMinter);
+    }
+
+    /**
+     * @notice Set the new admin.
+     * @dev After all is set up, admin only can change the token name
+     * @param _admin New admin address
+     */
+    function setAdmin(address _admin) external onlyAdmin {
+        admin = _admin;
+        emit SetAdmin(_admin);
+    }
+
+    /**
+     * @notice Mint `_value` tokens and assign them to `_to`
+     * @dev Emits a Transfer event originating from 0x00
+     * @param _to The account that will receive the created tokens
+     * @param _value The amount that will be created
+     * @return bool success
+     */
+    function mint(address _to, uint256 _value) external returns (bool) {
+        require(msg.sender == tokenMinter, "dev: tokenMinter only"); // dev: tokenMinter only
+        require(_to != address(0), "dev: zero address"); // dev: zero address
+
+        if (block.timestamp >= startEpochTime + rateReductionTime) {
+            _updateMiningParameters();
+        }
+        require(
+            totalSupply() + _value <= _availableSupply(),
+            "dev: exceeds allowable mint amount"
+        );
+
+        _mint(_to, _value);
+
+        return true;
+    }
+
+    /**
+     * @notice Burn `_value` tokens belonging to `msg.sender`
+     * @dev Emits a Transfer event with a destination of 0x00
+     * @param _value The amount that will be burned
+     * @return bool success
+     */
+    function burn(uint256 _value) external returns (bool) {
+        _burn(msg.sender, _value);
+        return true;
+    }
+
+    modifier onlyAdmin() {
+        require(admin == msg.sender, "dev: admin only");
+        _;
+    }
+}
