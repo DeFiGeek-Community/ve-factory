61b24e4824fa86ad3d60e876c9b7cc71e9e18853
naizo10 <naizo018767@gmail.com>
2024-06-05 15:21:04 +0900

    Update DEPLOYED_VETOKENS_STORAGE_LOCATION constant

--- src/Storage/Storage.sol
+++ src/Storage/Storage.sol
@@ -6,7 +6,7 @@ import {Schema} from "./Schema.sol";
 library Storage {
     // bytes32 private constant DEPLOYED_VETOKENS_STORAGE_LOCATION = keccak256(abi.encode(uint256(keccak256("VeFactory.VeTokenInfo")) - 1)) & ~bytes32(uint256(0xff));
     bytes32 private constant DEPLOYED_VETOKENS_STORAGE_LOCATION =
-        0x034eab1f967b01acc29ed43b575543f84affebe55494d54d0d4700702700f900;
+        0x965167c5566e6400ca5c0b84cde19d419bf7efdf30963b12dce3259d1e4b8d00;
 
     function deployedVeTokens()
         internal
