2e8086f2161e14c5f4bda5edb187990b57cdf627
naizo10 <naizo018767@gmail.com>
2024-06-07 13:32:22 +0900

    Add Storage.sol with feeDistributor function

---
+++ src/Storage/Storage.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {FeeDistributorSchema} from "./FeeDistributorSchema.sol";
+
+library Storage {
+    // ERC-7201に基づく名前空間ID "fees.distributor.main"のストレージ位置を計算
+    // keccak256(abi.encode(uint256(keccak256("fees.distributor.main")) - 1)) & ~bytes32(uint256(0xff));
+    bytes32 private constant FEE_DISTRIBUTOR_STORAGE_LOCATION =
+        0x965167c5566e6400ca5c0b84cde19d419bf7efdf30963b12dce3259d1e4b8d11;
+
+    function feeDistributor()
+        internal
+        pure
+        returns (FeeDistributorSchema.Storage storage s)
+    {
+        assembly {
+            s.slot := FEE_DISTRIBUTOR_STORAGE_LOCATION
+        }
+    }
+}
