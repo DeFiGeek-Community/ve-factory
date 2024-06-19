0388562d0e6d52d5972571da403d671c495bf530
naizo10 <naizo018767@gmail.com>
2024-06-19 19:24:30 +0900

    Update FeeDistributorClaimManyTest with new functionality

--- test/FeeDistributor/claimMany.test.sol
+++ test/FeeDistributor/claimMany.test.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.24;
 
 import "forge-std/Test.sol";
+import {MCTest} from "@mc/devkit/Flattened.sol";
 import "src/FeeDistributor.sol";
+import "src/Interfaces/IFeeDistributor.sol";
 import "src/VeToken.sol";
 import "src/test/SampleToken.sol";
 
-contract FeeDistributorClaimManyTest is Test {
+contract FeeDistributorClaimManyTest is MCTest {
     uint256 constant DAY = 86400;
     uint256 constant WEEK = DAY * 7;
     uint256 constant amount = 1e18 * 1000; // 1000 tokens
@@ -14,6 +16,8 @@ contract FeeDistributorClaimManyTest is Test {
     address alice;
     address bob;
     address charlie;
+
+    IFeeDistributor public feeDistributor = IFeeDistributor(target);
 
     FeeDistributor distributor;
     VeToken veToken;
@@ -25,13 +29,31 @@ contract FeeDistributorClaimManyTest is Test {
         bob = address(0x2);
         charlie = address(0x3);
 
-        token = new SampleToken(1e20);
-        coinA = new SampleToken(1e20);
+        token = new SampleToken(1e26);
+        coinA = new SampleToken(1e26);
         veToken = new VeToken(address(token), "veToken", "veTKN");
-        uint256 currentTime = block.timestamp;
-        distributor = new FeeDistributor(
+        distributor = new FeeDistributor();
+
+        _use(FeeDistributor.initialize.selector, address(distributor));
+        _use(
+            FeeDistributor.checkpointTotalSupply.selector,
+            address(distributor)
+        );
+        _use(FeeDistributor.claimMany.selector, address(distributor));
+        _use(FeeDistributor.checkpointToken.selector, address(distributor));
+        _use(FeeDistributor.timeCursor.selector, address(distributor));
+        _use(FeeDistributor.veSupply.selector, address(distributor));
+        _use(bytes4(keccak256("claim()")), address(distributor));
+        _use(bytes4(keccak256("claim(address)")), address(distributor));
+        _use(FeeDistributor.lastTokenTime.selector, address(distributor));
+        _use(
+            FeeDistributor.toggleAllowCheckpointToken.selector,
+            address(distributor)
+        );
+
+        feeDistributor.initialize(
             address(veToken),
-            currentTime,
+            block.timestamp,
             address(coinA),
             alice,
             bob
@@ -41,30 +63,28 @@ contract FeeDistributorClaimManyTest is Test {
         token.transfer(bob, amount);
         token.transfer(charlie, amount);
 
+        vm.prank(alice);
         token.approve(address(veToken), amount * 10);
         vm.prank(bob);
         token.approve(address(veToken), amount * 10);
         vm.prank(charlie);
         token.approve(address(veToken), amount * 10);
 
+        vm.prank(alice);
         veToken.createLock(amount, block.timestamp + 8 * WEEK);
         vm.prank(bob);
-        veToken.createLock(
-            amount,
-            block.timestamp + 8 * WEEK
-        );
+        veToken.createLock(amount, block.timestamp + 8 * WEEK);
         vm.prank(charlie);
-        veToken.createLock(
-            amount,
-            block.timestamp + 8 * WEEK
-        );
+        veToken.createLock(amount, block.timestamp + 8 * WEEK);
 
         vm.warp(block.timestamp + WEEK * 5);
 
-        coinA.transfer(address(distributor), 1e18 * 10);
-        distributor.checkpointToken();
+        coinA.transfer(address(feeDistributor), 1e18 * 10);
+
+        vm.startPrank(alice);
+        feeDistributor.checkpointToken();
         vm.warp(block.timestamp + WEEK);
-        distributor.checkpointToken();
+        feeDistributor.checkpointToken();
     }
 
     function testClaimMany() public {
@@ -77,7 +97,7 @@ contract FeeDistributorClaimManyTest is Test {
         uint256 balanceBeforeBob = coinA.balanceOf(bob);
         uint256 balanceBeforeCharlie = coinA.balanceOf(charlie);
 
-        distributor.claimMany(claimants);
+        feeDistributor.claimMany(claimants);
 
         uint256 balanceAfterAlice = coinA.balanceOf(alice);
         uint256 balanceAfterBob = coinA.balanceOf(bob);
@@ -96,10 +116,12 @@ contract FeeDistributorClaimManyTest is Test {
 
         uint256 balanceBefore = coinA.balanceOf(alice);
 
-        distributor.claimMany(claimants);
+        feeDistributor.claimMany(claimants);
 
         uint256 balanceAfter = coinA.balanceOf(alice);
 
         assertTrue(balanceAfter > balanceBefore);
     }
+
+    receive() external payable {}
 }
