afc457f3db245454ec91e4cd2f392c68e0ef3030
naizo10 <naizo018767@gmail.com>
2024-07-12 14:54:14 +0900

    Refactor IMultiTokenFeeDistributor interface

--- src/Interfaces/IMultiTokenFeeDistributor.sol
+++ src/Interfaces/IMultiTokenFeeDistributor.sol
@@ -2,12 +2,42 @@
 pragma solidity ^0.8.24;
 
 interface IMultiTokenFeeDistributor {
-   // Initialization and administrative functions
     function initialize(
         address votingEscrow_,
         address admin_,
         address emergencyReturn_
     ) external;
+
+    function checkpointToken(address tokenAddress_) external;
+
+    function veForAt(
+        address user_,
+        uint256 timestamp_
+    ) external view returns (uint256);
+
+    function checkpointTotalSupply() external;
+
+    function claim(address tokenAddress_) external returns (uint256);
+
+    function claim(
+        address userAddress_,
+        address tokenAddress_
+    ) external returns (uint256);
+
+    function claimMany(
+        address[] memory receivers_,
+        address tokenAddress_
+    ) external returns (bool);
+
+    function claimMultipleTokens(
+        address[] calldata tokenAddresses
+    ) external returns (bool);
+
+    function burn(address tokenAddress_) external returns (bool);
+
+    function addToken(address tokenAddress_, uint256 startTime_) external;
+
+    function removeToken(address tokenAddress_) external;
 
     function commitAdmin(address addr_) external;
 
@@ -17,32 +47,20 @@ interface IMultiTokenFeeDistributor {
 
     function killMe() external;
 
-    function recoverBalance(address token_) external returns (bool);
-
-    // Token management functions
-    function addToken(address token_, uint256 startTime_) external;
-
-    function removeToken(address token_) external;
-
-    function checkpointToken(address token_) external;
-
-    function burn(address token_) external returns (bool);
-
-    // Claiming and distribution functions
-    function claim(address token_) external returns (uint256);
-
-    function claimMultipleTokens(address[] calldata tokens_) external returns (bool);
-
-    function checkpointTotalSupply() external;
+    function recoverBalance(address tokenAddress_) external returns (bool);
 
     // View functions for storage variables
     function startTime() external view returns (uint256);
 
     function timeCursor() external view returns (uint256);
 
-    function lastTokenTime(address token_) external view returns (uint256);
+    function lastTokenTime(
+        address tokenAddress
+    ) external view returns (uint256);
 
-    function tokenLastBalance(address token_) external view returns (uint256);
+    function tokenLastBalance(
+        address tokenAddress
+    ) external view returns (uint256);
 
     function canCheckpointToken() external view returns (bool);
 
@@ -50,20 +68,28 @@ interface IMultiTokenFeeDistributor {
 
     function votingEscrow() external view returns (address);
 
+    function tokens() external view returns (address[] memory);
+
     function admin() external view returns (address);
 
     function futureAdmin() external view returns (address);
 
     function emergencyReturn() external view returns (address);
 
-    function tokens() external view returns (address[] memory);
+    function timeCursorOf(
+        address tokenAddress,
+        address user
+    ) external view returns (uint256);
 
-    // Additional view functions specific to MultiTokenFeeDistributor
-    function tokensPerWeek(address token_, uint256 week) external view returns (uint256);
+    function userEpochOf(
+        address tokenAddress,
+        address user
+    ) external view returns (uint256);
+
+    function tokensPerWeek(
+        address tokenAddress,
+        uint256 week
+    ) external view returns (uint256);
 
     function veSupply(uint256 week) external view returns (uint256);
-
-    function timeCursorOf(address token_, address user) external view returns (uint256);
-
-    function userEpochOf(address token_, address user) external view returns (uint256);
 }
