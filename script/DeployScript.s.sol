e4d9c207ae60f3b153350aca458533206c034850
naizo10 <naizo018767@gmail.com>
2024-09-16 12:43:08 +0900

    Refactor DeployFeeDistributor with UcsDeployLibrary

--- script/DeployScript.s.sol
+++ script/DeployScript.s.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {MCDevKit} from "@mc/devkit/MCDevKit.sol";
-import {MCScript} from "@mc/devkit/MCScript.sol";
 import "forge-std/console.sol";
 import "src/FeeDistributor.sol";
 import "src/Interfaces/FeeDistributorFacade.sol";
+import {UcsDeployLibrary} from "./UcsDeployLibrary.sol";
+import {DeployBase} from "./DeployBase.sol";
 
-contract DeployFeeDistributor is MCScript {
-    function run() public startBroadcastWith("DEPLOYER_PRIV_KEY") {
+contract DeployFeeDistributor is DeployBase {
+    function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         require(deployerPrivateKey != 0, "PRIVATE_KEY is not set");
 
@@ -27,69 +27,62 @@ contract DeployFeeDistributor is MCScript {
         address emergencyReturn = vm.envAddress("EMERGENCY_RETURN");
         require(emergencyReturn != address(0), "EMERGENCY_RETURN is not set");
 
-        // vm.startBroadcast(deployerPrivateKey);
+        vm.startBroadcast(deployerPrivateKey);
 
         // deploy関数に環境変数から読み込んだ引数を渡す
         deploy(votingEscrow, startTime, token, admin, emergencyReturn);
 
-        // vm.stopBroadcast();
+        vm.stopBroadcast();
     }
 
-    function deploy(
-        address votingEscrow,
-        uint256 startTime,
-        address token,
-        address admin,
-        address emergencyReturn
-    ) internal returns (address) {
+    function deploy(address votingEscrow, uint256 startTime, address token, address admin, address emergencyReturn)
+        internal
+        returns (address)
+    {
         // 初期化関数とその引数をエンコード
-        bytes memory initializerData = abi.encodeCall(
-            FeeDistributor.initialize,
-            (votingEscrow, startTime, token, admin, emergencyReturn)
-        );
+        bytes memory initializerData =
+            abi.encodeCall(FeeDistributor.initialize, (votingEscrow, startTime, token, admin, emergencyReturn));
         FeeDistributor distributor = new FeeDistributor();
+        writeDeployedAddress(address(distributor), "FeeDistributor_Impl");
 
-        mc.init("FeeDistributor");
-        mc.use(FeeDistributor.initialize.selector, address(distributor));
-        mc.use(FeeDistributor.checkpointToken.selector, address(distributor));
-        mc.use(FeeDistributor.veForAt.selector, address(distributor));
-        mc.use(
-            FeeDistributor.checkpointTotalSupply.selector,
-            address(distributor)
-        );
-        mc.use(bytes4(keccak256("claim()")), address(distributor));
-        mc.use(bytes4(keccak256("claim(address)")), address(distributor));
-        mc.use(FeeDistributor.claimMany.selector, address(distributor));
-        mc.use(FeeDistributor.burn.selector, address(distributor));
-        mc.use(FeeDistributor.commitAdmin.selector, address(distributor));
-        mc.use(FeeDistributor.applyAdmin.selector, address(distributor));
-        mc.use(
-            FeeDistributor.toggleAllowCheckpointToken.selector,
-            address(distributor)
-        );
-        mc.use(FeeDistributor.killMe.selector, address(distributor));
-        mc.use(FeeDistributor.recoverBalance.selector, address(distributor));
-        mc.use(FeeDistributor.startTime.selector, address(distributor));
-        mc.use(FeeDistributor.timeCursor.selector, address(distributor));
-        mc.use(FeeDistributor.lastTokenTime.selector, address(distributor));
-        mc.use(FeeDistributor.totalReceived.selector, address(distributor));
-        mc.use(FeeDistributor.tokenLastBalance.selector, address(distributor));
-        mc.use(
-            FeeDistributor.canCheckpointToken.selector,
-            address(distributor)
-        );
-        mc.use(FeeDistributor.isKilled.selector, address(distributor));
-        mc.use(FeeDistributor.votingEscrow.selector, address(distributor));
-        mc.use(FeeDistributor.token.selector, address(distributor));
-        mc.use(FeeDistributor.admin.selector, address(distributor));
-        mc.use(FeeDistributor.futureAdmin.selector, address(distributor));
-        mc.use(FeeDistributor.emergencyReturn.selector, address(distributor));
-        mc.use(FeeDistributor.timeCursorOf.selector, address(distributor));
-        mc.use(FeeDistributor.userEpochOf.selector, address(distributor));
-        mc.use(FeeDistributor.tokensPerWeek.selector, address(distributor));
-        mc.use(FeeDistributor.veSupply.selector, address(distributor));
-        mc.useFacade(address(new FeeDistributorFacade()));
-        address proxyAddress = mc.deploy(initializerData).toProxyAddress();
+        // UcsDeployLibraryを使用してデプロイ
+        address dictionary = UcsDeployLibrary.deployDictionary(admin);
+        writeDeployedAddress(dictionary, "FeeDistributor_Dictionary");
+
+        UcsDeployLibrary.use(dictionary, FeeDistributor.initialize.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.checkpointToken.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.veForAt.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.checkpointTotalSupply.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, bytes4(keccak256("claim()")), address(distributor));
+        UcsDeployLibrary.use(dictionary, bytes4(keccak256("claim(address)")), address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.claimMany.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.burn.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.commitAdmin.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.applyAdmin.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.toggleAllowCheckpointToken.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.killMe.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.recoverBalance.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.startTime.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.timeCursor.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.lastTokenTime.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.totalReceived.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.tokenLastBalance.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.canCheckpointToken.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.isKilled.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.votingEscrow.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.token.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.admin.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.futureAdmin.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.emergencyReturn.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.timeCursorOf.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.userEpochOf.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.tokensPerWeek.selector, address(distributor));
+        UcsDeployLibrary.use(dictionary, FeeDistributor.veSupply.selector, address(distributor));
+        UcsDeployLibrary.useFacade(dictionary, address(new FeeDistributorFacade()));
+
+        address proxyAddress = UcsDeployLibrary.deployProxy(dictionary, initializerData);
+        writeDeployedAddress(address(distributor), "FeeDistributor_Proxy");
+
         console.log("Deployed VeFactory proxy at:", proxyAddress);
         return proxyAddress;
     }
