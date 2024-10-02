26f7fe33fc99e13b7eff0dc3e571f7ef2ca87f9f
naizo10 <naizo018767@gmail.com>
2024-10-02 21:20:42 +0900

    Update tests for MultiTokenFeeDistributor init

--- test/MultiTokenFeeDistributor/initialize.test.sol
+++ test/MultiTokenFeeDistributor/initialize.test.sol
@@ -2,21 +2,21 @@
 pragma solidity ^0.8.24;
 
 import "test/util/TestBase.sol";
-import "src/FeeDistributor.sol";
-import "src/Interfaces/IFeeDistributor.sol";
+import "src/MultiTokenFeeDistributor.sol";
+import "src/Interfaces/IMultiTokenFeeDistributor.sol";
 import "src/VeToken.sol";
 import "src/test/SampleToken.sol";
 
-contract FeeDistributorInitializeTest is TestBase {
+contract MultiTokenFeeDistributorInitializeTest is TestBase {
     uint256 constant DAY = 86400;
     uint256 constant WEEK = DAY * 7;
 
     address alice;
     address bob;
 
-    IFeeDistributor public feeDistributor = IFeeDistributor(target);
+    IMultiTokenFeeDistributor public feeDistributor = IMultiTokenFeeDistributor(target);
 
-    FeeDistributor distributor;
+    MultiTokenFeeDistributor distributor;
     VeToken veToken;
     IERC20 token;
     SampleToken coinA;
@@ -27,42 +27,33 @@ contract FeeDistributorInitializeTest is TestBase {
 
         token = new SampleToken(1e32);
         veToken = new VeToken(address(token), "veToken", "veTKN");
-        distributor = new FeeDistributor();
+        distributor = new MultiTokenFeeDistributor();
 
-        _use(FeeDistributor.initialize.selector, address(distributor));
-        _use(FeeDistributor.votingEscrow.selector, address(distributor));
-        _use(FeeDistributor.startTime.selector, address(distributor));
-        _use(FeeDistributor.lastTokenTime.selector, address(distributor));
-        _use(FeeDistributor.timeCursor.selector, address(distributor));
-        _use(FeeDistributor.token.selector, address(distributor));
-        _use(FeeDistributor.admin.selector, address(distributor));
-        _use(FeeDistributor.emergencyReturn.selector, address(distributor));
+        _use(MultiTokenFeeDistributor.initialize.selector, address(distributor));
+        _use(MultiTokenFeeDistributor.votingEscrow.selector, address(distributor));
+        _use(MultiTokenFeeDistributor.startTime.selector, address(distributor));
+        _use(MultiTokenFeeDistributor.lastTokenTime.selector, address(distributor));
+        _use(MultiTokenFeeDistributor.timeCursor.selector, address(distributor));
+        _use(MultiTokenFeeDistributor.admin.selector, address(distributor));
+        _use(MultiTokenFeeDistributor.emergencyReturn.selector, address(distributor));
     }
 
     function testInitialize() public {
         vm.prank(alice);
-        uint256 startTime = block.timestamp;
-        feeDistributor.initialize(address(veToken), startTime, address(coinA), alice, bob);
-
-        uint256 time = (startTime / WEEK) * WEEK;
+        feeDistributor.initialize(address(veToken), alice, bob);
 
         assertEq(feeDistributor.votingEscrow(), address(veToken));
-        assertEq(feeDistributor.startTime(), time);
-        assertEq(feeDistributor.lastTokenTime(), time);
-        assertEq(feeDistributor.timeCursor(), time);
-        assertEq(feeDistributor.token(), address(coinA));
         assertEq(feeDistributor.admin(), alice);
         assertEq(feeDistributor.emergencyReturn(), bob);
     }
 
     function testInitializeMultipleTimesReverts() public {
-        uint256 startTime = block.timestamp;
-        distributor.initialize(address(veToken), startTime, address(token), alice, bob);
+        feeDistributor.initialize(address(veToken), alice, bob);
 
         vm.expectRevert(abi.encodeWithSignature("InvalidInitialization()"));
-        distributor.initialize(address(veToken), startTime, address(token), alice, bob);
+        feeDistributor.initialize(address(veToken), alice, bob);
 
         vm.expectRevert(abi.encodeWithSignature("InvalidInitialization()"));
-        distributor.initialize(address(veToken), startTime, address(token), alice, bob);
+        feeDistributor.initialize(address(veToken), alice, bob);
     }
 }
