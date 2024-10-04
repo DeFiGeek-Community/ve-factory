a4747e18ee29f020a4111a5762ec2482d108ff97
naizo10 <naizo018767@gmail.com>
2024-10-04 17:41:24 +0900

    Rename and reorder functions in IFeeDistributor interface

--- src/Interfaces/IFeeDistributor.sol
+++ src/Interfaces/IFeeDistributor.sol
@@ -40,9 +40,9 @@ interface IFeeDistributor {
 
     function timeCursor() external view returns (uint256);
 
-    function lastTokenTime() external view returns (uint256);
+    function lastCheckpointTotalSupplyTime() external view returns (uint256);
 
-    function totalReceived() external view returns (uint256);
+    function lastTokenTime() external view returns (uint256);
 
     function tokenLastBalance() external view returns (uint256);
 
