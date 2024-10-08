67e222eaa7c9af375fdd68c4884ca0f727db2a42
naizo10 <naizo018767@gmail.com>
2024-10-08 13:15:06 +0900

    Update import path for Upgrades.sol in DeployVeFactory

--- script/DeployVeFactory.s.sol
+++ script/DeployVeFactory.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
-import "lib/openzeppelin-foundry-upgrades/src/Upgrades.sol";
+import "openzeppelin-foundry-upgrades/Upgrades.sol";
 import "src/VeFactory.sol";
 
 contract DeployVeFactory is Script {
